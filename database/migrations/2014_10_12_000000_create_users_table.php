f6631e6d582771ccb2be6cc96aba005330b0a78b
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-17 21:08:05 +0100

    migration and seeder for the User table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('date_of_birth');
-            $table->boolean('role')->default(0);
+            $table->date('date_of_birth');
+            $table->boolean('user_type')->default(0);
+            $table->text('about')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,26 +15,31 @@ class UserSeeder extends Seeder
     {
         $users = [
             [
-                'name' => 'us1',
+                'first_name' => 'us1',
+                'last_name' => 'us1',
                 'email' => 'us1@us.it',
                 'password' => Hash::make('user1'),
-                'date_of_birth' => '26-02-1990',
-                'role' => 0,
+                'date_of_birth' => '1990-02-26',
+                'user_type' => 0,
+                'about' => 'Reach more buyers and drive higher conversion with the only payments platform that delivers PayPal, Venmo (in the US), credit and debit cards, and'
             ],
             [
-                'name' => 'us2',
+                'first_name' => 'us2',
+                'last_name' => 'us2',
                 'email' => 'us2@us.it',
                 'password' => Hash::make('user2'),
-                'date_of_birth' => '26-02-1990',
-                'role' => 1,
-
+                'date_of_birth' => '1990-05-14',
+                'user_type' => 1,
+                'about' => 'Reach more buyers and drive higher conversion with the only payments platform that delivers PayPal, Venmo (in the US), credit and debit cards, and'
             ],
             [
-                'name' => 'us3',
+                'first_name' => 'us3',
+                'last_name' => 'us3',
                 'email' => 'us3@us.it',
                 'password' => Hash::make('user3'),
-                'date_of_birth' => '26-02-1990',
-                'role' => 1,
+                'date_of_birth' => '1990-07-16',
+                'user_type' => 1,
+                'about' => 'Reach more buyers and drive higher conversion with the only payments platform that delivers PayPal, Venmo (in the US), credit and debit cards, and'
 
             ]
         ];
