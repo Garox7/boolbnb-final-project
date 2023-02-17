f2145daa7830750a4996c69d4dffbb52873784b5
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-17 21:53:53 +0100

    migration and seeder for the Properties table

---
+++ database/migrations/2023_02_16_104326_create_properties_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->string('address', 150);
+            $table->string('latitude', 50)->nullable();
+            $table->string('longitude', 50)->nullable();
+            $table->string('bedroom_count', 10);
+            $table->string('bed_count', 10);
+            $table->string('bathroom_count', 10);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
---
+++ database/seeds/PropertySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Property;
+use App\User;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class PropertySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $faker = Faker::create('it_IT');
+        $userId = User::all('id')->all();
+
+        for ($i = 0; $i < 20; $i++) {
+            Property::create([
+                'name' => $faker->words(rand(2, 5), true),
+                'description' => $faker->paragraphs(rand(2, 4), true),
+                'user_id' => $faker->randomElement($userId)->id,
+                'address' => $faker->address(),
+                'latitude' => $faker->latitude(),
+                'longitude' => $faker->longitude(),
+                'bedroom_count' => rand(1, 20),
+                'bed_count' => rand(1, 20),
+                'bathroom_count' => rand(1, 20),
+                'status' => rand(0, 1),
+            ]);
+        }
+    }
+}
