407f7093b8264c66fd56985a5f28d97cb2df4850
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-21 16:16:26 +0100

    first frontend side navigation setup

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!-- header  -->
+        <HeaderComponent />
 
         <router-view />
 
@@ -9,11 +9,38 @@
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+
 export default {
     name: 'App',
+    components: {
+        HeaderComponent,
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+// TODO: creare un file di reset
+/* Scegliere il font */
 
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+:root {
+    /********** colors ***********/
+    --body-color: #FFF;
+    --text-color: #222;
+    // boolean ispiration
+    --button-color: #3535ec;
+    // airbnb original
+    // --button-color: #ff395c;
+}
+
+body {
+    height: 100%;
+    background: var(--body-color);
+}
 </style>
