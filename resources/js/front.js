558cbd25ce31077a03aed8cd1429d2aeab8fb234
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-23 00:08:59 +0100

    removed bootstrap from frontend side

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,9 +1,11 @@
-require('./common');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
 import PageHome from './pages/PageHome';
+import PageProperty from './pages/PageProperty';
 
 Vue.use(VueRouter);
 
@@ -12,6 +14,11 @@ const routes = [
         path: '/',
         name: 'pageHome',
         component: PageHome,
+    },
+    {
+        path: '/properties',
+        name: 'PageProperty',
+        component: PageProperty,
     }
 ];
 
