fb91edbd37a0cd2a13f21fe0aaa850c9837870ed
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-23 22:08:21 +0100

    fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/front.js', 'public/js')
-mix.js('resources/js/back.js', 'public/js')
+    .js('resources/js/back.js', 'public/js')
     .vue()
     .sass('resources/sass/back.scss', 'public/css')
     .options({
